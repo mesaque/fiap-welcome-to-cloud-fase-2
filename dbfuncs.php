5865141fcf7d466d3428e8046d92cbe4dbabec20
Mesaque S. Silva <mesaque.s.silva@gmail.com>
2021-02-23 20:52:38 -0300

    add common functions

---
+++ dbfuncs.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = getenv("DB_HOST");
+$dbname = getenv("DB_NAME");
+$dbuser = getenv("DB_USER");
+$dbpassword = getenv("DB_PASSWORD");
+
+$_SESSION['link'] = new mysqli($host, $dbuser, $dbpassword, $dbname);
+
+if ($link->connect_error) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+function getSelect($sql){
+	$result = $_SESSION['link']->query($sql);
+	if( $result  ) {
+		return array( $result->fetch_row() );
+	}
+	return false;
+}
---
+++ mysql-statements.sql
@@ -0,0 +1,12 @@
+USE fiap2;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username varchar(60) NOT NULL,
+    password varchar(255),
+    firstname varchar(50) NOT NULL,
+    lastname varchar(50) NOT NULL,
+    email varchar(100)
+)  ENGINE=INNODB;
+
+INSERT INTO users(username,password,firstname,lastname,email) VALUES('caio', '30c303c6735967f37dd48156df91e5ecdf83674deac374c08adcd497663d7e19', 'Caio', 'Carvalho', 'CaioMeloCarvalho@teleworm.us');
