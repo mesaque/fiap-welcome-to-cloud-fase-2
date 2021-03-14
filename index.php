757fe751840b9596a89a063835fb677e7ce0211f
Mesaque Silva <mesaque.s.silva@gmail.com>
2021-03-14 13:19:41 -0300

    Update index.php
    comentários para elucidas as soluções de segurança

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
+// Obfuscamento de Erros para evitar falhas como por exemplo: path disclosure 
 ini_set('error_reporting', E_ALL & ~E_DEPRECATED & ~E_STRICT & ~E_NOTICE);
 ini_set('display_errors', 0);
 ini_set('log_errors', 0);
 
 require_once 'dbfuncs.php';
 
+// Removido o $_REQUEST para um método único reduzindo a superfície de ataque
 $getUser = $_GET["username"];
+// como é ID, forçado ser sempre int
 $getId  = intval( $_GET["id"] );
 
 $results = false;
 
 if(!empty($getUser)) {
+	
+	//bind param para evitar SQL INJECTION
 	$query  = $dbConnection->prepare('SELECT * FROM users WHERE username = ?');
-
+	
 	$query->bind_param('s', $getUser);
 	$results = getSelect($query);
 }
 elseif(!empty($getId)) {
+	
+	//bind param para evitar SQL INJECTION
 	$query  = $dbConnection->prepare('SELECT * FROM users WHERE id = ?');
 	$query->bind_param('i', $getId);
 
@@ -25,6 +32,7 @@ elseif(!empty($getId)) {
 
 
 if(!$results) {
+	//Saída tratar XSS
 	printf("Unable to find users: %s", htmlspecialchars($getUser, ENT_QUOTES, 'UTF-8') );
 }
 else {
